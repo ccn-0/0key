ee2bbed886f7ffcb0c0c35009d4b0e5ebbd999b7
matusadam <adam.matus@seznam.cz>
2023-01-20 18:52:54 +0100

    dont look

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-#include "context.h"
-
 #include <Windows.h>
-
+#include <stdexcept>
 #include <conio.h>
 #include <chrono>
 #include <thread>
@@ -9,11 +7,37 @@
 #include <iomanip>
 #include <deque>
 #include <map>
+#include <random>
+#include "interception.h"
 
-#define GRACE_PERIOD 60 // strafe switch can happen again [time in ms]
+/* Strafe switch can happen again [time in ms] */
+#define GRACE_PERIOD 70 
 
+/* Controls how inaccurate can key switches happen
+ * Higher number = more inaccurate */
+#define KEYPRESS_RANDOM_DEVIATION 2
 
-context ctx;
+class context {
+public:
+    context();
+    ~context();
+
+    inline operator InterceptionContext() const noexcept { return context_; }
+
+private:
+    InterceptionContext context_;
+};
+
+context::context() {
+    context_ = interception_create_context();
+    if (!context_) {
+        throw std::runtime_error("failed to create interception context");
+    }
+}
+
+context::~context() {
+    interception_destroy_context(context_);
+}
 
 enum ScanCode
 {
@@ -35,22 +59,11 @@ struct entry {
         , stroke(ims)
     {}
 };
-std::deque<entry> history;
 
-void thread_delay()
-{
-    while (true) {
-        while (!history.empty()) {
-            interception_send(ctx, history.front().device, reinterpret_cast<InterceptionStroke*>(&history.front().stroke), 1);
-            history.pop_front();
-            //std::this_thread::sleep_for(std::chrono::milliseconds(6));
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(2));
-    }
-}
+context ctx;
 
 
-int speed_limit(int speed) 
+int speed_limit(int speed)
 {
     // Precomputed soft MSL: 
     // for x in range(64):
@@ -58,18 +71,18 @@ int speed_limit(int speed)
     int pre_f[] = { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 8, 9, 9, 10, 10, 11, 11, 11, 12, 12, 12, 13, 13, 13, 13, 13, 14, 14, 14, 14, 14, 14, 15,
         15, 15, 15, 15, 15, 15, 15, 15, 15, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 17, 17, 17
     };
-    if (speed >= 0) 
+    if (speed >= 0)
     {
-        if (speed < 64) 
+        if (speed < 64)
         {
             speed = pre_f[speed];
         }
     }
-    else 
+    else
     {
-        if (speed > -64) 
+        if (speed > -64)
         {
-            speed = pre_f[speed*-1] * -1;
+            speed = pre_f[speed * -1] * -1;
         }
     }
     return speed;
@@ -77,9 +90,15 @@ int speed_limit(int speed)
 
 int main() {
 
-	std::cout << "MSL 2022";
-
-    //std::thread(thread_delay).detach();
+    std::cout << " ________  ___  __    _______       ___    ___ \n";
+    std::cout << "|\\   __  \\|\\  \\|\\  \\ |\\  ___ \\     |\\  \\  /  /|\n";
+    std::cout << "\\ \\  \\|\\  \\ \\  \\/  /|\\ \\   __/|    \\ \\  \\/  / /\n";
+    std::cout << " \\ \\  \\\\\\  \\ \\   ___  \\ \\  \\_|/__   \\ \\    / / \n";
+    std::cout << "  \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \\  \\_|\\ \\   \\/  /  /  \n";
+    std::cout << "   \\ \\_______\\ \\__\\\\ \\__\\ \\_______\\__/  / /    \n";
+    std::cout << "    \\|_______|\\|__| \\|__|\\|_______|\\___/ /     \n";
+    std::cout << "                                  \\|___|/      \n";
+    std::cout << "                                               \n";
 
     SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
 
@@ -89,6 +108,10 @@ int main() {
     unsigned int strafeDir = 0; // 0: no strafing, 1: left, 2: right
 
     int strafeSwitchSpeed = 3; // this is used to preemptively switch strafes
+
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+    std::uniform_int_distribution<> distr(-KEYPRESS_RANDOM_DEVIATION, KEYPRESS_RANDOM_DEVIATION); // define the range
 
     InterceptionDevice device; // device 1 is keyboard, device 11 is mouse
     InterceptionStroke stroke;
@@ -130,30 +153,33 @@ int main() {
 
             interception_send(ctx, device, &stroke, 1);
 
-            if (mstroke.rolling == -120) 
+            if (mstroke.rolling == -120)
             { // MWHEELDOWN
                 probablyAir = true;
                 interception_send(ctx, 1, (InterceptionStroke*)&fake_w_end, 1);
             }
 
-            if (probablyAir) 
+            if (probablyAir)
             {
                 auto t_now = std::chrono::high_resolution_clock::now();
+
                 if (std::chrono::duration<double, std::milli>(t_now - t_last).count() > GRACE_PERIOD) {
-                    if (mstroke.x >= (-1 * strafeSwitchSpeed) && strafeDir != 2)
+                    int rand_deviation = distr(gen);
+                    int switchspd = (strafeSwitchSpeed + rand_deviation);
+                    switchspd = switchspd < 0 ? 0 : switchspd;
+                    std::cout << switchspd << " ";
+                    if (mstroke.x >= (-1 * switchspd) && strafeDir != 2)
                     {
                         strafeDir = 2;
                         interception_send(ctx, 1, (InterceptionStroke*)&fake_a_end, 1);
                         interception_send(ctx, 1, (InterceptionStroke*)&fake_d, 1);
-                        history.emplace_back(1, fake_d);
                         t_last = t_now;
                     }
-                    else if (mstroke.x <= strafeSwitchSpeed && strafeDir != 1)
+                    else if (mstroke.x <= switchspd && strafeDir != 1)
                     {
                         strafeDir = 1;
                         interception_send(ctx, 1, (InterceptionStroke*)&fake_d_end, 1);
                         interception_send(ctx, 1, (InterceptionStroke*)&fake_a, 1);
-                        //history.emplace_back(1, fake_a);
                         t_last = t_now;
                     }
                 }
@@ -162,7 +188,7 @@ int main() {
         }
         else if (interception_is_keyboard(device))
         {
-            InterceptionKeyStroke& kstroke = *(InterceptionKeyStroke*)&stroke;    
+            InterceptionKeyStroke& kstroke = *(InterceptionKeyStroke*)&stroke;
 
             if (probablyAir) // Ignore all actual A and D inputs to not mess with the emulator
             {
@@ -191,7 +217,7 @@ int main() {
 
             if (kstroke.code == SCANCODE_LEFT && kstroke.state == 0)
             {
-                strafeSwitchSpeed = strafeSwitchSpeed > 0 ? strafeSwitchSpeed-1 : strafeSwitchSpeed;
+                strafeSwitchSpeed = strafeSwitchSpeed > 0 ? strafeSwitchSpeed - 1 : strafeSwitchSpeed;
                 std::cout << "Speed threshold for strafe switch [-]: " << strafeSwitchSpeed << std::endl;
             }
 
